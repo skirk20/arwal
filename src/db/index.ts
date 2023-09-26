646f91c930e9a6975954a939405197dc9b5919d5
skirk20 <lukasjanaszek@gmail.com>
2023-09-26 12:07:50 +0200

    poprawki

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,10 @@
 import mysql from "mysql2";
 import dbConfig from "../config/db.config";
+
+
 export default mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB
-  });
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
+});
